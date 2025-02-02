6ebe7fac0a9a746f19680b4f28a628de80a540e1
Mushood Mohammed Hanif <supame123@gmail.com>
2025-02-03 03:22:14 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import "./src/env.ts";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+   experimental: {
+      serverActions: {
+         bodySizeLimit: "3mb",
+      },
+   },
+};
 
 export default config;
